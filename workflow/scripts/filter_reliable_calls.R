ba40f0dfaa265d718cf33b9e9947c86ce04ac37b
Johannes Hausmann <johannes.hausmann@posteo.de>
2025-05-08 07:08:33 +0200

    Update reliable call filtering

--- workflow/scripts/filter_reliable_calls.R
+++ workflow/scripts/filter_reliable_calls.R
@@ -7,10 +7,16 @@ suppressMessages({
 
 df <- readr::read_tsv(snakemake@input[['annotated_sj']], show_col_types = FALSE)
 
+df <- df %>%
+  dplyr::mutate(
+    reliable_call = intron_jaccard >= snakemake@params[["min_junction_usage"]] &
+                    jCPM_uniquely_mapped >= snakemake@params[["min_junction_cpm"]]
+  )
+
 df %>%
-  dplyr::filter(intron_jaccard < snakemake@params[['min_junction_usage']]) %>%
+  dplyr::filter(!reliable_call) %>%
   readr::write_tsv(snakemake@output[['sj_low_expression']])
 
 df %>%
-  dplyr::filter(intron_jaccard >= snakemake@params[['min_junction_usage']]) %>%
+  dplyr::filter(reliable_call) %>%
   readr::write_tsv(snakemake@output[['sj_expression']])
