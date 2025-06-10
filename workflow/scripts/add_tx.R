4f987b31b68cb58ed5b924b1085b1f256f9fa554
Jo Hausmann <35309108+johausmann@users.noreply.github.com>
2025-06-10 09:52:01 +0200

    Update add_tx.R
    Remove future_map from splice2neo operation

--- workflow/scripts/add_tx.R
+++ workflow/scripts/add_tx.R
@@ -5,23 +5,8 @@ suppressMessages({
   library(magrittr)
   library(splice2neo)
   library(GenomicFeatures)
-  library(furrr)
   library(purrr)
 })
-options(future.fork.enable = TRUE)
-plan(multicore, workers = as.integer(snakemake@threads))
-#gene_exclusion <- function(df, exclusion_pattern) {
-#  #stopifnot("hgnc" %in% colnames(df), "Column HGNC is missing")
-#  exclusion_pattern <- readr::read_tsv(exclusion_pattern, show_col_types = FALSE)
-#  
-#  df <- df %>%
-#    fuzzyjoin::regex_left_join(exclusion_pattern,
-#      by = c("hgnc" = "exclude_gene_pattern")
-#    ) %>%
-#    dplyr::mutate(exclude_gene =!is.na(exclude_gene_pattern))
-#  return(df)
-#
-#}
 
 transcripts <- base::readRDS(snakemake@input[['transcripts']])
 bsg <- rtracklayer::TwoBitFile(snakemake@input[['genome']])
@@ -30,14 +15,11 @@ df <- readr::read_tsv(snakemake@input[['parsed_sj']], show_col_types = FALSE)
 
 df <- df %>%
   dplyr::filter(!exclude_gene) %>%
-  dplyr::filter(!is.na(gene_id)) %>% 
-  base::split(.$junc_id) %>% 
-  furrr::future_map(
-    ~splice2neo::add_context_seq(.x, 
+  dplyr::filter(!is.na(gene_id)) %>%
+  splice2neo::add_context_seq(.,
       transcripts = transcripts,
       size = snakemake@params[['cts_size']],
       bsg = bsg)
   )
-df <- dplyr::bind_rows(discard(df, ~nrow(.x) == 0))
 
 df %>% readr::write_tsv(snakemake@output[['annotated_sj']])
